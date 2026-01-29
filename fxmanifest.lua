822fad4be09aa59107c344dddd360c4862b8c72b
PokjadAnaqi <alypheyjad1@gmail.com>
2026-01-30 03:32:14 +0800

    Update author and version in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,11 @@
 fx_version 'cerulean'
 use_experimental_fxv2_oal 'yes'
 lua54 'yes'
-
 game 'gta5'
 
-author 'Renewed Scripts | PokjadRafaey'
+author 'Renewed Scripts | Rewrite by PokjadRafaey'
 description 'Renewed Weaponscarry'
-version '3.0'
+version '3.0.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
