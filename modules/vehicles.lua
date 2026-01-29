10cf0a5fa5f2c780bd843e8fbdea5e9ddd277fd3
PokjadAnaqi <alypheyjad1@gmail.com>
2026-01-29 19:02:49 +0800

    Update vehicles.lua
    Fix the weapon prop so that it doesn’t get removed when you enter vehicles.

--- modules/vehicles.lua
+++ modules/vehicles.lua
@@ -1,18 +1,16 @@
-
 local Utils = require 'modules.utils'
 local Vehicles = {}
 
 CreateThread(function()
     while true do
-
-        for bagname, _ in pairs(Vehicles) do
-            local baggedVehicles = Vehicles[bagname]
-
+        for bagname, baggedVehicles in pairs(Vehicles) do
             if baggedVehicles and table.type(baggedVehicles) ~= 'empty' then
-                for entity, props in pairs(baggedVehicles) do
-                    if not DoesEntityExist(entity) and props and table.type(props) ~= 'empty' then
-                        Utils.removeEntities(props)
-                        Vehicles[bagname][entity] = nil
+                for vehicle, props in pairs(baggedVehicles) do
+                    if not DoesEntityExist(vehicle) then
+                        if props and table.type(props) ~= 'empty' then
+                            Utils.removeEntities(props)
+                        end
+                        Vehicles[bagname][vehicle] = nil
                     end
                 end
             end
@@ -22,73 +20,84 @@ CreateThread(function()
 end)
 
 local function attachObject(item, entity, vehicle)
-    local pos, rot = item.pos, item.rot
+    if not item or not entity or not vehicle then return end
+    if not DoesEntityExist(entity) or not DoesEntityExist(vehicle) then return end
 
-    if pos and rot then
-        AttachEntityToEntity(entity, vehicle, item.bone and GetEntityBoneIndexByName(entity, item.bone) or 0, pos.x, pos.y, pos.z, rot.x, rot.y, rot.z, true, true, true, false, 5, true)
-    end
+    local pos, rot = item.pos, item.rot
+    if not pos or not rot then return end
+
+    AttachEntityToEntity(
+        entity,
+        vehicle,
+        item.bone and GetEntityBoneIndexByName(vehicle, item.bone) or 0,
+        pos.x, pos.y, pos.z,
+        rot.x, rot.y, rot.z,
+        true, true, false, false, 2, true
+    )
 end
 
-
-local function createAllObjects(vehicle, addItems, currentTable, amount)
+local function createAllObjects(vehicle, addItems, currentTable)
     for i = 1, #addItems do
         local item = addItems[i]
         local object = Utils.getEntity(item)
 
-        SetEntityCollision(object, false, false)
+        if object and object > 0 and DoesEntityExist(object) then
+            -- make sure entity is active and safe
+            FreezeEntityPosition(object, false)
+            SetEntityDynamic(object, true)
+            SetEntityCollision(object, false, false)
+            SetEntityVisible(object, true, false)
+            ActivatePhysics(object)
 
-        attachObject(item, object, vehicle)
-        amount += 1
-        currentTable[amount] = {
-            name = item.name,
-            entity = object,
-        }
+            attachObject(item, object, vehicle)
+
+            currentTable[#currentTable + 1] = {
+                name = item.name,
+                entity = object,
+            }
+        else
+            print(('[CarryVehicle] Failed to create entity for %s'):format(item.name or 'unknown'))
+        end
     end
 end
 
 local function addVehicleStateBag(name)
     AddStateBagChangeHandler(name, '', function(bagName, keyName, value, _, replicated)
-        if replicated then
-            return
+        if replicated then return end
+
+        local vehicle = Utils.getEntityFromStateBag(bagName, keyName)
+        if not vehicle or vehicle == 0 or not DoesEntityExist(vehicle) then
+            return -- silently ignore invalid vehicle instead of crashing
         end
 
-        local entity = Utils.getEntityFromStateBag(bagName, keyName)
+        Vehicles[keyName] = Vehicles[keyName] or {}
+        local currentTable = Vehicles[keyName][vehicle] or {}
 
-        if entity == 0 then
-            return error(('%s received invalid entity! (%s)'):format(keyName, bagName))
+        -- remove old props
+        if table.type(currentTable) ~= 'empty' then
+            Utils.removeEntities(currentTable)
+            table.wipe(currentTable)
         end
 
-        if entity then
-            if not Vehicles[keyName] then
-                Vehicles[keyName] = {}
-            end
-
-            local currentTable = Vehicles[keyName][entity] or {}
-
-            if table.type(currentTable) ~= 'empty' then
-                Utils.removeEntities(currentTable)
-                table.wipe(currentTable)
-            end
-
-            if value and next(value) then
-               createAllObjects(entity, value, currentTable, 0)
-            end
-
-            Vehicles[keyName][entity] = currentTable
+        -- create new props
+        if value and table.type(value) ~= 'empty' then
+            -- small delay to ensure vehicle fully streamed
+            Wait(50)
+            createAllObjects(vehicle, value, currentTable)
         end
+
+        Vehicles[keyName][vehicle] = currentTable
     end)
-
 end
 
 AddEventHandler('onResourceStop', function(resource)
-    if resource == cache.resource then
+    if resource ~= cache.resource then return end
 
-        for bagname, _ in pairs(Vehicles) do
-            if Vehicles[bagname] and table.type(Vehicles[bagname]) ~= 'empty' then
-                for _, props in pairs(Vehicles[bagname]) do
-                    if props and table.type(props) ~= 'empty' then
-                        Utils.removeEntities(props)
-                    end
+    for _, baggedVehicles in pairs(Vehicles) do
+        if baggedVehicles and table.type(baggedVehicles) ~= 'empty' then
+            for _, props in pairs(baggedVehicles) do
+                if props and table.type(props) ~= 'empty' then
+                    Utils.removeEntities(props)
                 end
             end
         end
